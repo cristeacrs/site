6dc31315e233969f3e315e95190e730343a7357e
cristeacrs <cristea.cristi@yahoo.com>
2021-11-25 01:44:58 +0200

    am adaugat js si css

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("eu sunt Cristi");
+let listaPargrafe = document.getElementsByTagName("p");
+console.log(listaPargrafe)
+listaPargrafe[1].innerHTML="mie nu mi place sa salut , mie mi place sa merg mai departe";
+alert("buna dimineata");
+let raspuns = prompt("esti de acord cum procedeaza Maria?");
+alert("Raspunsul userului este :" + raspuns);
